d4334080c6f3d82ed2dfe2dc786723a5b6510ed2
Feroz Ahmmed <feroz.shah.940@gmail.com>
2022-07-21 17:53:39 +0600

    adding lombok

--- src/main/java/com/tutorial/sultana/entities/Mans.java
+++ src/main/java/com/tutorial/sultana/entities/Mans.java
@@ -1,5 +1,7 @@
 package com.tutorial.sultana.entities;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -8,6 +10,8 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+@Getter
+@Setter
 @Document(collection= "mans")
 public class Mans {
     @Id
@@ -17,47 +21,4 @@ public class Mans {
     private int rollNamber;
     private List<String> teacher=new ArrayList<>();
 
-    public List<String> getTeacher() {
-        return teacher;
-    }
-
-    public void setTeacher(List<String> teacher) {
-        this.teacher = teacher;
-    }
-
-
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getCllass() {
-        return cllass;
-    }
-
-    public void setCllass(String cllass) {
-        this.cllass = cllass;
-    }
-
-    public int getRollNamber() {
-        return rollNamber;
-    }
-
-    public void setRollNamber(int rollNamber) {
-        this.rollNamber = rollNamber;
-    }
-
-
-
-    public ObjectId getId() {
-        return Id;
-    }
-
-    public void setId(ObjectId id) {
-        Id = id;
-    }
 }
--- src/main/java/com/tutorial/sultana/entities/Persons.java
+++ src/main/java/com/tutorial/sultana/entities/Persons.java
@@ -1,13 +1,16 @@
 package com.tutorial.sultana.entities;
 
 
+import lombok.Getter;
+import lombok.Setter;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.ArrayList;
 import java.util.List;
-
+@Getter
+@Setter
 @Document(collection = "persons")
 public class Persons {
     @Id
@@ -20,60 +23,4 @@ public class Persons {
     private String address;
     private List<String> friends=new ArrayList<>();
 
-
-    public List<String> getFriends() {
-        return friends;
-    }
-
-    public void setFriends(List<String> friends) {
-        this.friends = friends;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getFatherName() {
-        return fatherName;
-    }
-
-    public void setFatherName(String fatherName) {
-        this.fatherName = fatherName;
-    }
-
-    public String getMotherName() {
-        return motherName;
-    }
-
-    public void setMotherName(String motherName) {
-        this.motherName = motherName;
-    }
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
